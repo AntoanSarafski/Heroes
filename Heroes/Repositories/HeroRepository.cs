283aa529b0875106fb95147618f0a7d95fdd8e94
Antoan Sarafski <antoansarafski@gmail.com>
2023-05-03 14:56:20 +0300

    Commit with HeroRepository class.

---
+++ Heroes/Repositories/HeroRepository.cs
@@ -0,0 +1,38 @@
+﻿using Heroes.Models.Contracts;
+using Heroes.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Heroes.Repositories
+{
+    internal class HeroRepository : IRepository<IHero>
+    {
+        private List<IHero> heroes;
+        public IReadOnlyCollection<IHero> Models => heroes.AsReadOnly();
+
+        public void Add(IHero model)
+        {
+            heroes.Add(model);
+        }
+
+
+        public bool Remove(IHero model)
+        {
+            if (heroes.Remove(model))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public IHero FindByName(string name)
+            => heroes.FirstOrDefault(h => h.Name == name);
+
+    }
+}
