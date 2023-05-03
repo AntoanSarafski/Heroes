5b0e8b3875380a09cb1217cb3b792304141d60c9
Antoan Sarafski <antoansarafski@gmail.com>
2023-05-03 14:34:43 +0300

    Commit with more validators.

--- Heroes/Models/Heroes/Hero.cs
+++ Heroes/Models/Heroes/Hero.cs
@@ -1,10 +1,84 @@
-﻿using System;
+﻿using Heroes.Models.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Heroes.Models.Heroes
 {
-    internal class Hero
+    public abstract class Hero : IHero
     {
+        private string name;
+        private int health;
+        private int armour;
+        private IWeapon weapon;
+        private bool isAlive;
+
+        public string Name 
+        { 
+            get => name;
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Hero name cannot be null or empty.");
+                }
+                name = value;
+            }
+        }
+
+        public int Health 
+        {
+            get => health;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Hero health cannot be below 0.");
+                }
+                health = value;
+            } 
+        }
+
+        public int Armour 
+        {
+            get => armour;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Hero armour cannot be below 0.");
+                }
+                armour = value;
+            }
+        }
+
+        public IWeapon Weapon
+        {
+            get => weapon;
+            private set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentException("Weapon cannot be null.");
+                }
+                weapon = value;
+            } 
+        }
+
+        public bool IsAlive 
+        { 
+            get => isAlive;
+            private set => isAlive = value;
+        }
+
+        public void AddWeapon(IWeapon weapon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TakeDamage(int points)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
